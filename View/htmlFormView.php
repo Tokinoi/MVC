a9434804e71b5597b3f6da90de454903ffbb5836
Tokinoi <alonzo.mathon@outlook.com>
2024-11-25 13:54:34 +0100

    First commit - All controller in the same files

---
+++ View/htmlFormView.php
@@ -0,0 +1,6 @@
+<h2>Ajouter un article</h2>
+    <form method="POST" action="">
+        <input type="text" name="titre" placeholder="Titre de l'article" required>
+        <textarea name="contenu" rows="5" placeholder="Contenu de l'article" required></textarea>
+        <button type="submit">Publier</button>
+    </form>
